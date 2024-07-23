67e033200e1f296390974a64af7481e13935d30f
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-07-23 13:50:26 +0530

    Revamped the JobForm
    Changed the whole layout and added an text editor in the form.

--- src/Components/JobDetails.jsx
+++ src/Components/JobDetails.jsx
@@ -1,17 +1,42 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function JobDetails() {
+  const navigate = useNavigate();
   const located = useLocation();
-  const { job } = located.state;
-  const {title, description, skills, location } = job;
-  return (
+  const { formData } = located.state;
+  const {
+    jobTitle,
+    jobType,
+    experience,
+    applicationDeadline,
+    jobDescription,
+    minSalary,
+    maxSalary,
+    jobTags,
+    location,
+    qualifications,} = formData;
+    function handleClick(){
+      navigate(-1);
+    }
+    return (
+   
     <div>
       <h2>Job Details</h2>
-      <p><strong>Job Title:</strong> {title}</p>
-      <p><strong>Skills:</strong> {skills}</p>
-      <p><strong>Description:</strong> {description}</p>
+      <p><strong>Job Title:</strong> {jobTitle}</p>
+      <p><strong>Job Type:</strong> {jobType}</p>
+      <p><strong>Skills:</strong> {jobTags}</p>
+      <p><strong>Description:</strong> {jobDescription}</p>
       <p><strong>Job Location:</strong> {location}</p>
+      <p><strong>Job Type:</strong>{jobType}</p>
+      <p><strong>Experience Level:</strong> {experience}</p>
+      <p><strong>Min Salary :</strong> {minSalary}</p>
+      <p><strong>Max Salary :</strong> {maxSalary}</p>
+      <p><strong>Application Deadline:</strong> {applicationDeadline}</p>
+      <p><strong>Qualification:</strong> {qualifications}</p>
+
+      <button onClick={handleClick}
+      >Edit</button>
     </div>
   );
 }
--- src/Components/JobForm.jsx
+++ src/Components/JobForm.jsx
@@ -1,74 +1,241 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-function JobForm() {
-  const [job, setJob] = useState({
-    title: '',
-    skills: '',
-    description: '',
-    location: '',
-  });
+import Dropdown from 'react-bootstrap/Dropdown';
+import { Form, Button, Row, Col } from 'react-bootstrap';
+import JoditEditor from 'jodit-react';
+// function JobForm() {
+//   const [job, setJob] = useState({
+//     title: '',
+//     skills: '',
+//     description: '',
+//     location: '',
+//     employType: '',
+//     appDeadline: '',
+//     salRange: '',
+//     Experience: '',
+//     education: '',
 
+//   });
+
+//   const navigate = useNavigate();
+//   const [loading, setLoading] = useState(false);
+
+
+
+
+
+//   const generateDescription = async () => {
+//     setLoading(true);
+//     try {
+//       const response = await axios.post('https://End-point', {
+//         title: job.title,
+//         skills: job.skills,
+//       });
+//       setJob({
+//         ...job,
+//         description: response.data.description,
+//       });
+//     } catch (error) {
+//       console.error('Error fetching job description:', error);
+//     }
+//     setLoading(false);
+//   };
+
+
+
+
+//         <div>
+//           <label>Description:</label>
+//           <textarea name="description" value={job.description} onChange={handleChange} required />
+//           <button type="button" onClick={generateDescription} disabled={loading}>
+//             {loading ? 'Generating...' : 'Generate Description'}
+//           </button>
+//         </div>
+//         <div>
+
+const JobForm = () => {
+
+
+  const editor = useRef(null)
   const navigate = useNavigate();
-  const [loading, setLoading] = useState(false);
-  
-  
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setJob({
-      ...job,
-      [name]: value,
+  const [formData, setFormData] = useState({
+    jobTitle: '',
+    jobType: '',
+    experience: '',
+    applicationDeadline: '',
+    jobDescription: '',
+    minSalary: '',
+    maxSalary: '',
+    jobTags: '',
+    location: '',
+    qualifications: ''
+  });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    navigate('/job-details', { state: { formData } });
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
     });
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    navigate('/job-details', { state: { job } });
-  };
-  const generateDescription = async () => {
-    setLoading(true);
-    try {
-      const response = await axios.post('https://End-point', {
-        title: job.title,
-        skills: job.skills,
-      });
-      setJob({
-        ...job,
-        description: response.data.description,
-      });
-    } catch (error) {
-      console.error('Error fetching job description:', error);
-    }
-    setLoading(false);
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log(formData);
+  // };
 
   return (
-    <div>
-      <h2>Create a Job Listing</h2>
-      <form onSubmit={handleSubmit}>
+    <div className='container mx-auto p-4 '>
+
+
+      <h1 className='blue-400 fs-1 mb-4 font'>Post A New Job </h1>
+      <Form onSubmit={handleSubmit}>
+        <Row className="mb-3">
+          <Col>
+            <Form.Group controlId="formJobTitle">
+
+              <Form.Control
+                type="text"
+                name="jobTitle"
+                value={formData.jobTitle}
+                onChange={handleChange}
+                placeholder="Job Title"
+                className='$blue-200' />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="JobType">
+
+              <Form.Control
+                as="select"
+                name="jobType"
+                value={formData.jobType}
+                onChange={handleChange}>
+                <option value="Freelancer">Freelancer</option>
+                <option value="Remote">Remote</option>
+                <option value="On-Site">On-Site</option>
+                <option value="Hybrid">Hybrid</option>
+              </Form.Control>
+            </Form.Group>
+          </Col>
+        </Row>
+
+        <Row className="mb-3">
+          <Col>
+            <Form.Group controlId="formExperience">
+
+              <Form.Control
+                type="text"
+                name="experience"
+                value={formData.experience}
+                onChange={handleChange}
+                placeholder="Enter Experience"
+              />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="formApplicationDeadline">
+
+              <Form.Control
+                type="date"
+                name="applicationDeadline"
+                value={formData.applicationDeadline}
+                onChange={handleChange}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+
         <div>
-          <label>Job Title:</label>
-          <input type="text" name="title" value={job.title} onChange={handleChange} required />
+        <JoditEditor 
+          className='mb-4 mt-4'
+          ref={editor}
+          value={formData.jobDescription}
+          onChange={handleChange}
+        />
+          {/* <Form.Group controlId="formJobDescription" className="mb-3 ">
+
+            <Form.Control
+              as="textarea"
+              name="jobDescription"
+              value={formData.jobDescription}
+              onChange={handleChange}
+              placeholder="Enter Job Description"
+              rows={3}
+            />
+          </Form.Group> */}
         </div>
-        <div>
-          <label>Skills:</label>
-          <input type="text" name="skills" value={job.skills} onChange={handleChange} required />
-        </div>
-        <div>
-          <label>Description:</label>
-          <textarea name="description" value={job.description} onChange={handleChange} required />
-          <button type="button" onClick={generateDescription} disabled={loading}>
-            {loading ? 'Generating...' : 'Generate Description'}
-          </button>
-        </div>
-        <div>
-          <label>Job Location:</label>
-          <input type="text" name="location" value={job.location} onChange={handleChange} required />
-        </div>
-        <button type="submit">Create Job</button>
-      </form>
+
+        <Row className="mb-3">
+          <Col>
+            <Form.Group controlId="formMinSalary">
+
+              <Form.Control
+                type="number"
+                name="minSalary"
+                value={formData.minSalary}
+                onChange={handleChange}
+                placeholder="Enter Min Salary"
+              />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="formMaxSalary">
+
+              <Form.Control
+                type="number"
+                name="maxSalary"
+                value={formData.maxSalary}
+                onChange={handleChange}
+                placeholder="Enter Max Salary"
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+
+        <Form.Group controlId="formJobTags" className="mb-3">
+
+          <Form.Control
+            type="text"
+            name="jobTags"
+            value={formData.jobTags}
+            onChange={handleChange}
+            placeholder="e.g. PHP, Developer, CSS"
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formLocation" className="mb-3">
+
+          <Form.Control
+            type="text"
+            name="location"
+            value={formData.location}
+            onChange={handleChange}
+            placeholder="Enter Location"
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formQualifications" className="mb-3">
+
+          <Form.Control
+            type="text"
+            name="qualifications"
+            value={formData.qualifications}
+            onChange={handleChange}
+            placeholder="Enter Qualifications"
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Save & Preview
+        </Button>
+      </Form>
     </div>
   );
-}
+};
 
 export default JobForm;
