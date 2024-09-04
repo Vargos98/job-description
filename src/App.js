21c23b0d3dc43f6a18ae47de19ac047f741d399e
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-09-04 09:49:18 +0530

    Created basic details form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import JobFormTwo from './Components/JobFormTwo';
 import JobDetails from './Components/JobDetails';
 import ProfileSummary from './Components/ProfileSummary';
 import AwardCertification from './Components/AwardCertification';
+import BasicDetailsForm from './Components/BasicDetailsForm';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/job-details" element={<JobDetails />} />
         <Route path="/ProfileSummary" element={<ProfileSummary />} />
         <Route path="/JobFormTwo" element={<JobFormTwo />} />
+        <Route path="/BasicDetailsForm" element={<BasicDetailsForm />} />
     
       </Routes>
     </Router>
