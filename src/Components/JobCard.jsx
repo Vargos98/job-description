ce54dbe4398d9ec56c6207940c52e205054dbf4b
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-09-18 11:02:51 +0530

    Updated title for card

--- src/Components/JobCard.jsx
+++ src/Components/JobCard.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import Deloitte from '../Deloitte.png';
 const JobCard = () => {
   return (
-    <div className='p-4'>
+    <div className='p-4 font-sans'>
       <div className='w-[680px] h-[350px] bg-cyan-400 drop-shadow-2xl px-4 py-3 rounded'>
+        {/* Heading section */}
         <div className=' flex items-center gap-4'>
           <img className='w-[100px] h-[100px] bg-white rounded-md object-contain' src={Deloitte} alt={Deloitte} />
+
           <div className='title'>
             <h2 className='text-2xl font-bold '>Business Development Manager</h2>
             <p className='-mt-2 text-xl text-gray-500'>$50,000 - $75,000/Month</p>
+          </div>
+
+          <div className=''>
+            
           </div>
         </div>
       </div>
