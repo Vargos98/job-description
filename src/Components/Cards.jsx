53f38032f0ee51b6579f7f51f9d9f79f82fa0dcc
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-10-07 12:07:02 +0530

    Update Cards.jsx

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -3,12 +3,17 @@ import React from 'react'
 const Cards = () => {
 
   const data = [
-    { url: "https://images.unsplash.com/photo-1629904853893-c2c8981a1dc5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGRldmVsb3BlcnxlbnwwfHwwfHx8MA%3D%3D", name: "Developer Jobs" },
-    { url: "https://images.unsplash.com/photo-1607134541550-2994abb8077b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y29uc3RydWN0aW9uJTIwd29ya2VyfGVufDB8fDB8fHww", name: "Construction Jobs " },
-    { url: "https://images.unsplash.com/photo-1503387762-592deb58ef4e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8YXJjaGl0ZWN0fGVufDB8fDB8fHww", name: "Architect Jobs" },
-    { url: "https://images.unsplash.com/photo-1613909207039-6b173b755cc1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fFVJJTJGVVglMjBEZXNpZ25lcnxlbnwwfHwwfHx8MA%3D%3D", name: "Developer Jobs" },
-    { url: "https://images.unsplash.com/photo-1629904853893-c2c8981a1dc5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGRldmVsb3BlcnxlbnwwfHwwfHx8MA%3D%3D", name: "UI/UX Designers" },
-    { url: "https://images.unsplash.com/photo-1526948531399-320e7e40f0ca?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c2FsZXMlMjBqb2JzfGVufDB8fDB8fHww", name: "Sales Jobs" },
+    { url: "https://images.unsplash.com/photo-1629904853893-c2c8981a1dc5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGRldmVsb3BlcnxlbnwwfHwwfHx8MA%3D%3D", name: "Developer Jobs", posts:"5261"},
+
+    { url: "https://images.unsplash.com/photo-1607134541550-2994abb8077b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y29uc3RydWN0aW9uJTIwd29ya2VyfGVufDB8fDB8fHww", name: "Construction Jobs ", posts:"12512" },
+
+    { url: "https://images.unsplash.com/photo-1503387762-592deb58ef4e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8YXJjaGl0ZWN0fGVufDB8fDB8fHww", name: "Architect Jobs", posts:"3211" },
+
+    { url: "https://images.unsplash.com/photo-1613909207039-6b173b755cc1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fFVJJTJGVVglMjBEZXNpZ25lcnxlbnwwfHwwfHx8MA%3D%3D", name: "UI/UX Designers", posts:"7164" },
+
+    { url: "https://images.unsplash.com/photo-1629904853893-c2c8981a1dc5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGRldmVsb3BlcnxlbnwwfHwwfHx8MA%3D%3D", name: "Data Analyst Jobs", posts:"98112" },
+
+    { url: "https://images.unsplash.com/photo-1526948531399-320e7e40f0ca?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c2FsZXMlMjBqb2JzfGVufDB8fDB8fHww", name: "Sales Jobs", posts:"16406" },
   ]
 
 
@@ -22,14 +27,15 @@ const Cards = () => {
 
         {data.map((elem, index) => (
 
-          <div className='w-[90%] h-[30%] sm:w-full md:flex-col md:w-full lg:w-[15%]  md:h-[45%]   rounded-xl hover:w-[35%] lg:hover:w-[30%] md:hover:w-[80%] transition-all duration-[300ms] ease-out hover:ease-in relative overflow-hidden group' index={index}>
-            <h4 className='absolute text-center top-10 left-5 opacity-[1%] group-hover:opacity-100 bg-black rounded-md text-white text-lg sm:text-xl md:text-2xl lg:text-3xl z-20 transition-opacity duration-300'>
+          <div className='w-[90%] h-[30%] sm:w-full md:flex-col md:w-full lg:w-[15%]  md:h-[45%]   rounded-md hover:w-[35%] lg:hover:w-[30%] md:hover:w-[80%] transition-all duration-[300ms] ease-in hover:ease-in relative overflow-hidden group' index={index}>
+            <button className='absolute px-4 py-1 text-center top-10 left-5 opacity-[1%] group-hover:opacity-100 bg-black rounded-full text-white text-lg sm:text-xl md:text-2xl lg:text-3xl z-20 transition-opacity duration-300 text-nowrap ease-in'>
               {elem.name}
-            </h4>
-            <img className="object-cover w-full h-full grayscale hover:grayscale-0 rounded-xl" src={elem.url} alt="" />
+            </button>
+            <img className="object-cover w-full h-full grayscale hover:grayscale-0 rounded" src={elem.url} alt="" />
+            <button className='absolute px-4 py-1 font-semibold text-center bottom-10 right-5 opacity-[1%] group-hover:opacity-100 bg-white rounded-full text-black text-lg sm:text-xl md:text-2xl lg:text-lg z-20 transition-opacity duration-300 text-nowrap'>Jobs : {elem.posts}</button>
           </div>
         ))}
-
+        
       </div>
     </div>
   )
